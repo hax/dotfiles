4bc0f0ae8ea810abb3d58ff231083c0ab77ef7af
HE Shi-Jun <hax@heshijun.net>
2017-11-05 03:23:19 +0800

    fix tsconfig defaults

--- compress-tsconfig.js
+++ compress-tsconfig.js
@@ -10,6 +10,6 @@ const outPath = resolve(__dirname, './base.tsconfig.json')
 const banner = '// See https://github.com/hax/dotfiles/tree/master/src/tsconfig.yaml for full edition\n'
 const src = readFileSync(srcPath, 'utf-8')
 const content = safeLoad(src)
-const out = banner + JSON.stringify(content, null, '\t')
+const out = banner + JSON.stringify(content, null, '\t') + '\n'
 // console.log(out)
 writeFileSync(outPath, out, 'utf-8')
