22937e76061f7825da3bc2662729d972bc55c5b7
HE Shi-Jun <hax@heshijun.net>
2023-11-09 16:56:06 +0800

    regenerate ts config
    - update ts config as ts v5.3-beta
    - rename to tsconfig-base
    - drop old yaml, use json generated by tsc -init
    - compress ts config from json

--- compress-tsconfig.js
+++ compress-tsconfig.js
@@ -1,15 +1,18 @@
 'use strict'
 
-const {safeLoad} = require('js-yaml')
 const {readFileSync, writeFileSync} = require('fs')
 const {resolve} = require('path')
 
-const srcPath = resolve(__dirname, './src/tsconfig.yaml')
-const outPath = resolve(__dirname, './base.tsconfig.json')
+const srcPath = resolve(__dirname, './src/tsconfig-base.json')
+const outPath = resolve(__dirname, './tsconfig-base.json')
 
-const banner = '// See https://github.com/hax/dotfiles/tree/master/src/tsconfig.yaml for full edition\n'
+const banner = '// See https://github.com/hax/dotfiles/tree/master/src/tsconfig-base.json for full edition\n'
 const src = readFileSync(srcPath, 'utf-8')
-const content = safeLoad(src)
+const content = JSON.parse(removeComments(src))
 const out = banner + JSON.stringify(content, null, '\t') + '\n'
 // console.log(out)
 writeFileSync(outPath, out, 'utf-8')
+
+function removeComments(src) {
+	return src.replaceAll(/\/\/.*|\/\*[\s\S]*?\*\//g, "")
+}
